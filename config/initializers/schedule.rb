59bc3472acc566d51341a302012f39eb92c65ae3
Matias Araya <matias.arayac@gmail.com>
2015-11-01 19:23:22 -0300

    cada 10 minutos

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,7 +1,7 @@
 if defined?(Rails::Server)
   scheduler = Rufus::Scheduler.new
 
-  scheduler.every '5h' do
+  scheduler.every '10m' do
     $client.search("RT participa", result_type: "popular", lang: 'es').take(500).each do |tweet|
       puts "Message: #{tweet.text}"
       $client.retweet(tweet)            
